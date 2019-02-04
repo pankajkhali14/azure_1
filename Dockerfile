5c399865a5d69a6fb73a736445c543085835b1e9
pankajkhali14 <pankajkhali14@gmail.com>
2019-02-05 00:40:15 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:8.9.0-alpine
+
+WORKDIR /sandbox/learningdocker
+
+COPY package.json . 
+
+COPY package-lock.json . 
+
+RUN npm install
+
+COPY . .
+
+CMD ["npm", "start"]
+
+EXPOSE 4000
---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.use('*', (req, res, next) => {
+    res.send('<h1>Hello from express the updated app</h1>')
+})
+
+app.listen(4000, () => {
+    console.log('Express app listening on port 4000')
+})
